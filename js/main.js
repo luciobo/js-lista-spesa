8b365871bf254893edf02e905d355777d473f0f9
luciobo <luciobo@icloud.com>
2022-11-07 15:19:55 +0100

    commit iniziale

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+const listaSpesa = ["pane", "burro", "uova", "salmone", "birra",]
+console.log(listaSpesa)
+
+let i = 0;
+
+while (i < listaSpesa.length) {
+
+    i++;
+    console.log(i)
+
+}
+
+const listaSpesaEl = document.getElementsByClassName (".lista-spesa")
+console.log(listaSpesaEl)
+
+listaSpesaEl.i
