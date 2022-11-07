a856a0295f43ba43c20a17fdc4750ae9123b088e
luciobo <luciobo@icloud.com>
2022-11-07 18:19:28 +0100

    modifiche

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 
 
-const listaSpesa = ["pane", "burro", "uova", "salmone", "birra",]
-console.log(listaSpesa)
+const listaSpesa = ["pane", "burro", "uova", "salmone", "birra",];
+console.log(listaSpesa);
 
 let i = 0;
 
 while (i < listaSpesa.length) {
 
+    const prodottiSpesa = listaSpesa[i];
+    console.log(prodottiSpesa)
+
     i++;
     console.log(i)
 
-}
+};
 
+const listaSpesaEl = document.getElementById(lista);
+console.log(listaSpesaEl);
+
+listaSpesaEl.innerHTML = 
